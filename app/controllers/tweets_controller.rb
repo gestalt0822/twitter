2ca22058044ac67dfd81c513d1e0ca2a226393c8
AOKI Daisuke <dice.k.aoki@gmail.com>
2016-11-23 05:24:30 +0000

    fifth commit make set_blog

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  
+  before_action :set_tweet, only: [:edit, :update, :destroy]
 
   def index
     @tweets = Tweet.all
@@ -18,11 +20,11 @@ class TweetsController < ApplicationController
   end
   
   def edit
-    @tweet = Tweet.find(params[:id])
+
   end
   
   def update
-    @tweet = Tweet.find(params[:id])
+
     if @tweet.update(tweets_params)
       redirect_to tweets_path, notice: "投稿をへ編集しました！"
     else
@@ -31,14 +33,19 @@ class TweetsController < ApplicationController
   end
   
   def destroy
-    @tweet = Tweet.find(params[:id])
+ 
     @tweet.destroy
     redirect_to tweets_path
   end
   
   private
+  
     def tweets_params
       params.require(:tweet).permit(:content)
+    end
+    
+    def set_tweet
+      @tweet = Tweet.find(params[:id])
     end
   
 end
